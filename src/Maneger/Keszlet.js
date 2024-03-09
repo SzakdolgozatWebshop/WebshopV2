512cc36a9a7e4f76580c062fd60fd5ce4f21883a
SnowGRH <gergelyovinczeffy@gmail.com>
2024-03-09 15:01:03 +0100

    Rendeles,Keszlet table

--- src/Maneger/Keszlet.js
+++ src/Maneger/Keszlet.js
@@ -4,23 +4,68 @@ import React, { useState, useEffect } from "react";
 export default function Keszlet() {
   const [termek, setTermek] = useState([]);
   const [selectedTermek, setSelectedTermek] = useState([]);
+  const [formData, setFormData] = useState({
+    marka: "",
+    elnevezes: "",
+    keszlet: "",
+    eladasi_ar: "",
+  });
 
-  useEffect(() => {
-    fetch("http://localhost:8001/api/termekShow")
-      .then((response) => response.json())
-      .then((data) => setTermek(data))
-      .catch((error) => console.error("Error fetching users:", error));
-  }, []);
+  Toltes();
   const handleBovebbenClick = (termek) => {
     setSelectedTermek(termek);
     document.getElementById("LeirasModal").style.display = "block";
   };
-
+  useEffect(() => {
+    if (selectedTermek) {
+      setFormData({
+        marka: selectedTermek.marka || "",
+        elnevezes: selectedTermek.elnevezes || "",
+        keszlet: selectedTermek.keszlet || "",
+        eladasi_ar: selectedTermek.eladasi_ar || "",
+      });
+    }
+  }, [selectedTermek]);
   const closeModal = () => {
     setSelectedTermek([]);
     document.getElementById("LeirasModal").style.display = "none";
   };
-  console.log(termek);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:8001/api/keszlet/mod/${selectedTermek.ter_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+            console.log("Success:", data);
+            window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  function Toltes() {
+    useEffect(() => {
+      fetch("http://localhost:8001/api/termekShow")
+        .then((response) => response.json())
+        .then((data) => setTermek(data))
+        .catch((error) => console.error("Error fetching users:", error));
+    }, []);
+  }
+  console.log(formData);
   return (
     <div className="App">
       <Layout />
@@ -62,68 +107,75 @@ export default function Keszlet() {
         </table>
       </div>
       {selectedTermek && (
-        <form>
-        <div
-          id="LeirasModal"
-          className="modal bg-secondary p-3 text-dark bg-opacity-75"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h4 className="modal-title">Termék módosítás</h4>
-                <button type="button" className="btn-close"  onClick={closeModal}></button>
-              </div>
-              <div className="modal-body">
-                <p style={{ paddingTop: "5px" }}>
-                  Termék ID: {selectedTermek.ter_id}
-                </p>
-                <p style={{ paddingTop: "5px" }}>
-                  Márka Neve:
-                  <input
-                    type="text"
-                    placeholder={selectedTermek.marka}
-                    name="elnevezes"
-                  ></input>
-                </p>
-                <p style={{ paddingTop: "5px" }}>
-                  Termék Neve:
-                  <input
-                    type="text"
-                    placeholder={selectedTermek.elnevezes}
-                    name="elnevezes"
-                  ></input>
-                </p>
-                <p style={{ paddingTop: "5px" }}>
-                  Mennyiség:{" "}
-                  <input
-                    type="text"
-                    placeholder={selectedTermek.keszlet}
-                    name="keszlet"
-                  ></input>
-                </p>
-                <p style={{ paddingTop: "5px" }}>
-                  Eladási ár:{" "}
-                  <input
-                    type="text"
-                    placeholder={selectedTermek.eladasi_ar}
-                    name="keszlet"
-                  ></input>{" "}
-                  Ft
-                </p>
-                <div className="modal-footer">
-                  <p style={{ paddingTop: "10px" }}>
-                    <button type="submit" className="btn btn-danger">
-                      Küldés
-                    </button>
+        <form onSubmit={handleSubmit}>
+          <div
+            id="LeirasModal"
+            className="modal bg-secondary p-3 text-dark bg-opacity-75"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title">Termék módosítás</h4>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    onClick={closeModal}
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <p style={{ paddingTop: "5px" }}>
+                    Termék ID: {selectedTermek.ter_id}
                   </p>
+                  <p style={{ paddingTop: "5px" }}>
+                    Márka Neve:
+                    <input
+                      type="text"
+                      placeholder={formData.marka}
+                      onChange={handleInputChange}
+                      name="marka"
+                    ></input>
+                  </p>
+                  <p style={{ paddingTop: "5px" }}>
+                    Termék Neve:
+                    <input
+                      type="text"
+                      placeholder={formData.elnevezes}
+                      onChange={handleInputChange}
+                      name="elnevezes"
+                    ></input>
+                  </p>
+                  <p style={{ paddingTop: "5px" }}>
+                    Mennyiség:{" "}
+                    <input
+                      type="text"
+                      placeholder={formData.keszlet}
+                      onChange={handleInputChange}
+                      name="keszlet"
+                    ></input>
+                  </p>
+                  <p style={{ paddingTop: "5px" }}>
+                    Eladási ár:{" "}
+                    <input
+                      type="text"
+                      placeholder={formData.eladasi_ar}
+                      onChange={handleInputChange}
+                      name="eladasi_ar"
+                    ></input>{" "}
+                    Ft
+                  </p>
+                  <div className="modal-footer">
+                    <p style={{ paddingTop: "10px" }}>
+                      <button type="submit" className="btn btn-danger">
+                        Küldés
+                      </button>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </form>
       )}
-
     </div>
   );
 }
