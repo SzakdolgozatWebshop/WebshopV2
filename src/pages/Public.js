7e6925f61ce99e447f59717af3db07d4cb703565
Venc03 <vegi.dani1@gmail.com>
2024-03-22 15:21:08 +0100

    penteki ora, publik oldal

--- src/pages/Public.js
+++ src/pages/Public.js
@@ -5,9 +5,61 @@ function Public() {
   const { user } = useAuthContext();
   console.log(user);
 
+  const [termek, setTermek] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/termekShow")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setTermek(data))
+      .catch((error) => console.error("Error fetching termek data:", error));
+  }, []);
+
+
   return (
     <div className="app">
       <Layout />
+      <article>
+      <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+            <tr>
+              <th></th>
+            </tr>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                Válogatható Termékeink
+              </td>
+            </tr>
+            {termek.map((termeks) => (
+              <tr key={termeks.ter_id}>
+                <td>kep</td>
+              </tr>
+            ))}
+            {termek.map((termeks) => (
+              <tr key={termeks.ter_id}>
+                <td>{termeks.ter_id}</td>
+                <td>{termeks.marka}</td>
+                <td>{termeks.elnevezes}</td>
+                <td>{termeks.eladasi_ar} </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </article>
     </div>
   );
 }
