138242dffaaf6299c909a53405ce189738fb3a65
Venc03 <vegi.dani1@gmail.com>
2024-03-28 19:11:07 +0100

    publik oldal, index.css bovites

--- src/pages/Public.js
+++ src/pages/Public.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import Layout from "../Layout";
 import useAuthContext from "../context/AuthContext";
 
@@ -8,7 +9,7 @@ function Public() {
   const [termek, setTermek] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/termekShow")
+    fetch("http://localhost:8000/api/items")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -16,7 +17,9 @@ function Public() {
         return response.json();
       })
       .then((data) => setTermek(data))
-      .catch((error) => console.error("Error fetching termek data:", error));
+      .catch((error) => {
+        console.error("Error fetching termek data:", error);
+      });
   }, []);
 
 
@@ -24,43 +27,31 @@ function Public() {
     <div className="app">
       <Layout />
       <article>
-      <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th></th>
-            </tr>
-            <tr>
-              <th></th>
-            </tr>
-            <tr>
-              <th></th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                Válogatható Termékeink
-              </td>
-            </tr>
-            {termek.map((termeks) => (
-              <tr key={termeks.ter_id}>
-                <td>kep</td>
+        <div className='thatter'>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Termék ID</th>
+                <th>Márka</th>
+                <th>Elnevezés</th>
+                <th>Eladási Ár</th>
               </tr>
-            ))}
-            {termek.map((termeks) => (
-              <tr key={termeks.ter_id}>
-                <td>{termeks.ter_id}</td>
-                <td>{termeks.marka}</td>
-                <td>{termeks.elnevezes}</td>
-                <td>{termeks.eladasi_ar} </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {termek.map((termeks) => (
+                <tr key={termeks.ter_id}>
+                  <td>{termeks.ter_id}</td>
+                  <td>{termeks.marka}</td>
+                  <td>{termeks.elnevezes}</td>
+                  <td>{termeks.eladasi_ar}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </article>
     </div>
   );
 }
+
 export default Public;
